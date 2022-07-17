34f1e09da6967a05e8dd2ed2f3bc55db449c6086
Artur Lesnik <arturlesnik@gmail.com>
2022-07-17 15:40:09 +0200

    last commit today

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const btnNext = document.querySelector(".btn-next");
 const btnPrev = document.querySelector(".btn-prev");
 const imgs  = document.querySelectorAll("img")
+const slider = document.querySelector('slider')
+imgs.forEach((a,idx)=>{
+  console.log(a,idx)
+  a.style.transform=`translateX(${idx*100}%)`
+})
 
 function nextPhoto(){
-let mainSlide = document.querySelector('.main-slide')
-console.log(mainSlide.nextElementSibling)
+  console.log('test')
 }
-
 btnNext.addEventListener('click',nextPhoto)
