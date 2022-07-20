faae456850996d83a05b4f5c853b97a1ebeefb18
Artur Lesnik <arturlesnik@gmail.com>
2022-07-20 19:19:07 +0200

    update

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const btnNext = document.querySelector(".btn-next");
 const btnPrev = document.querySelector(".btn-prev");
 const imgs  = document.querySelectorAll("img")
-const slider = document.querySelector('slider')
+let currImg = 0;
+const limit = imgs.length-1
+
 imgs.forEach((a,idx)=>{
-  console.log(a,idx)
   a.style.transform=`translateX(${idx*100}%)`
 })
 
+
 function nextPhoto(){
-  console.log('test')
+  
+  if(currImg===limit){
+  currImg = 0
+  }
+  else{
+  currImg++
+  }
+  imgs.forEach((a,idx)=>{
+    a.style.transform=`translateX(${(idx-currImg)*100}%)`
+  })
 }
+
+function prevPhoto(){
+  console.log(limit)
+  console.log(currImg)
+  if(currImg===0){
+    currImg=limit
+  }
+  else{
+  currImg--
+  }
+  imgs.forEach((a,idx)=>{
+    a.style.transform=`translateX(${(idx-currImg)*100}%)`
+  })
+}
+
 btnNext.addEventListener('click',nextPhoto)
+btnPrev.addEventListener('click',prevPhoto)
